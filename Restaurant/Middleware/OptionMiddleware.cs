230d0cf7628c6955e018c5f542485d9345ee8307
Shepeliev Andrii <andrii.shepeliev@oski.site>
2024-06-11 18:33:57 +0300

    Removed logger in middleware

--- Restaurant/Middleware/OptionMiddleware.cs
+++ Restaurant/Middleware/OptionMiddleware.cs
@@ -3,33 +3,27 @@
 public class OptionsMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger _logger;
 
-    public OptionsMiddleware(RequestDelegate next, ILogger logger)
+    public OptionsMiddleware(RequestDelegate next)
     {
         _next = next;
-        _logger = logger;
     }
 
     public Task Invoke(HttpContext context)
     {
-        _logger.LogDebug("Start another");
         return BeginInvoke(context);
     }
 
     private Task BeginInvoke(HttpContext context)
     {
-        _logger.LogDebug("Start option middleware");
         if (context.Request.Method != "OPTIONS") return _next.Invoke(context);
 
-        _logger.LogDebug("Inside option middleware");
         context.Response.Headers.Add("Access-Control-Allow-Origin", new[] { (string)context.Request.Headers["Origin"]! });
         context.Response.Headers.Add("Access-Control-Allow-Headers", new[] { "Origin, X-Requested-With, Content-Type, Accept" });
         context.Response.Headers.Add("Access-Control-Allow-Methods", new[] { "GET, POST, PUT, DELETE, OPTIONS" });
         context.Response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
         context.Response.StatusCode = 200;
 
-        _logger.LogDebug("Response option middleware");
         return context.Response.WriteAsync("OK");
     }
 }
